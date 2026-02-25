e46eb83d1f5966ab0387a1b95ffcc15e2570cf10
ezazshabbir <ezazshabbir@gmail.com>
2026-02-25 22:25:03 +0530

    Update Base Url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5454',
+    baseURL: 'https://multivendorecommerce-production-000c.up.railway.app',
     headers: {
         'Content-Type': 'application/json',
     }
